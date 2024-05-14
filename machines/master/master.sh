464106c07d72aee22d66d6686b74e9b59d31644e
albertoagss <fp_aguilerajose@ciudadjardin.org>
2024-05-14 09:32:46 +0000

    Update master Vagrantfile

---
+++ machines/master/master.sh
@@ -0,0 +1,28 @@
+# Update packages
+sudo apt-get -y update && sudo apt-get -y upgrade
+
+# Remove old Python versions
+sudo apt -y remove python3
+
+# Install vim
+sudo apt-get -y install vim
+
+# Install Git
+sudo apt -y install git
+
+# Install Ansible
+sudo pip3 install ansible
+
+sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config;
+sudo systemctl restart sshd;
+
+# Change password for root user
+echo "root:1234" | sudo chpasswd
+
+# Change password for 'vagrant' user
+echo "vagrant:vagrant" | sudo chpasswd
+
+# Format CLI prompt
+echo "PS1='\e[1;32m\u@\h\e[m \e[1;36m\w $ \e[m'" | sudo tee -a /etc/bashrc
+echo "PS1='\e[0;31m\u@\h\e[m \e[1;36m\w\e[m\e[0;31m # \e[m'" | sudo tee -a /root/.bashrc
+source .bashrc
