53517a14130835bb0232dbfaa2dfb32efcc1fe94
Alberto Aguilera <fp_aguilerajose@ciudadjardin.org>
2024-05-26 22:25:55 +0200

    Comando vagrant up realizado en los directorios de las maquinas. De este modo, con clonar el repo ya podríamos levantar las máquinas, ahorrándonos crear cada directorio y sus vagrant inits correspondientes.

--- machines/master/master.sh
+++ machines/master/master.sh
@@ -4,14 +4,11 @@ sudo apt-get -y update && sudo apt-get -y upgrade
 # Remove old Python versions
 sudo apt -y remove python3
 
-# Install vim
-sudo apt-get -y install vim
-
-# Install Git
-sudo apt -y install git
+# Install all dependencies
+sudo apt-get -y install vim git python3.11 pip ansible
 
 # Install Ansible
-sudo pip3 install ansible
+# sudo pip3.11 install ansible
 
 # Enable SSH connection without password
 sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config;
@@ -21,7 +18,7 @@ sudo systemctl restart sshd;
 echo "root:1234" | sudo chpasswd
 
 # Change password for 'vagrant' user
-echo "vagrant:vagrant" | sudo chpasswd
+echo "alberto:1234" | sudo chpasswd
 
 # Format CLI prompt
 echo "PS1='\e[1;32m\u@\h\e[m \e[1;36m\w $ \e[m'" | sudo tee -a /etc/bashrc
