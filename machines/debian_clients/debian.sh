92d367111d8862535cd779e193bc2fd63c12e1ac
Alberto Aguilera <fp_aguilerajose@ciudadjardin.org>
2024-06-07 19:10:21 +0200

    Actualización de los scripts de aprovisionamiento de master y debian_clients.

--- machines/debian_clients/debian.sh
+++ machines/debian_clients/debian.sh
@@ -1,12 +1,20 @@
+# Update packages
 sudo apt-get -y update && sudo apt-get -y upgrade
 
+# Install all dependencies
 sudo apt-get -y install vim
 
+# Enable SSH connection without password
 sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config;
 sudo systemctl restart sshd;
 
+# Change password for 'root' user
 echo "root:1234" | sudo chpasswd
-echo "vagrant:vagrant" | sudo chpasswd
+
+# Change password for 'test_user' 
+echo "test_user:vagrant" | sudo chpasswd
+
+# Format CLI prompt
 echo "PS1='\e[1;32m\u@\h\e[m \e[1;36m\w $ \e[m'" | sudo tee -a /etc/bashrc
 echo "PS1='\e[0;31m\u@\h\e[m \e[1;36m\w\e[m\e[0;31m # \e[m'" | sudo tee -a /root/.bashrc
 source .bashrc
--- machines/master/master.sh
+++ machines/master/master.sh
@@ -7,18 +7,18 @@ sudo apt -y remove python3
 # Install all dependencies
 sudo apt-get -y install vim git python3.11 pip ansible
 
-# Install Ansible
-# sudo pip3.11 install ansible
+# Clone project repository
+git clone https://github.com/albertoagss/Proyecto-Final-ASIR.git
 
 # Enable SSH connection without password
 sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config;
 sudo systemctl restart sshd;
 
-# Change password for root user
+# Change password for 'root' user
 echo "root:1234" | sudo chpasswd
 
-# Change password for 'vagrant' user
-echo "alberto:1234" | sudo chpasswd
+# Change password for 'test_user' 
+echo "test_user:1234" | sudo chpasswd
 
 # Format CLI prompt
 echo "PS1='\e[1;32m\u@\h\e[m \e[1;36m\w $ \e[m'" | sudo tee -a /etc/bashrc
